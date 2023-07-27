7fd4e390fdd30964ae7df0e03e7766122cfd6284
Andriikryl <andreyk557@gmail.com>
2023-07-27 13:50:09 +0300

    add mainPage and userPage

--- social-project/src/App.jsx
+++ social-project/src/App.jsx
@@ -1,13 +1,15 @@
 import "./App.css"
 import {BrowserRouter, Route, Routes} from "react-router-dom"
+import MainPage from "./pages/MainPage"
+import UserPage from "./pages/UserPage"
 
 const App = () => {
   return (
     <div className="App">
        <BrowserRouter>
         <Routes>
-          <Route path="/" element={<div>page 1</div>}/>
-          <Route path="/:id" element={<div>user page</div>}/>
+          <Route path="/" element={<MainPage/>}/>
+          <Route path="/:id" element={<UserPage/>}/>
         </Routes>
        </BrowserRouter>
     </div>
